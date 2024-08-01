0bc3f5d6bb8b55e0dacfccd265f59f48530db284
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-08-01 11:14:40 +0530

    constructor

---
+++ JAVA/Constructor1.java
@@ -0,0 +1,10 @@
+public class Constructor1 {
+
+    Constructor1(){
+        System.out.println(this);
+    }
+    public static void main(String[] args) {
+        Constructor1 c1 = new Constructor1();
+        
+    }    
+}
