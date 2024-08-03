77b38529dfd2b7c022a1498997013350d2da8528
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-08-03 15:21:38 +0530

    input

--- JAVA/Exscanner.java
+++ JAVA/Exscanner.java
@@ -1,20 +1,14 @@
 import java.util.Scanner;
 
 public class Exscanner {
-
-    public static void printData(String nm, int a, char g){
-        System.out.println("Your Name is : "+ nm);
-        System.out.println("Your Age is : "+ a);
-        System.out.println("You are a : "+ g);
-    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Your Name : ");
-        String name = sc.nextLine();
-        System.out.println("Enter Your Age : ");
-        int age = sc.nextInt();
-        System.out.println("Enter Your Male : ");
-        char gen = sc.next().charAt(0);
-        printData(name, age, gen);
+        System.out.println("Enter Num :-");
+        int n = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Enter Text :-");
+        String s = sc.nextLine();
+        System.out.println("Text : "+ s);
+        System.out.println("Num : "+ n);
     }
 }
