35b7379390553cf3f8523988cece3047317f5d7a
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-08-03 15:24:18 +0530

    constructor

--- JAVA/Constructor1.java
+++ JAVA/Constructor1.java
@@ -1,10 +1,14 @@
 public class Constructor1 {
-
-    Constructor1(){
-        System.out.println(this);
+    String name;
+    int age;
+    Constructor1(String nm, int a){
+        // System.out.println(this);
+        this.name = nm;
+        this.age = a;
     }
     public static void main(String[] args) {
-        Constructor1 c1 = new Constructor1();
-        
+        Constructor1 c1 = new Constructor1("Bhargav", 21);
+        System.out.println(c1.name);
+        System.out.println(c1.age);
     }    
 }
