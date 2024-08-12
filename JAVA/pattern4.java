0500ad309ef905c03b109c418316f1c1ba8d7178
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-08-12 21:06:49 +0530

    no

--- JAVA/pattern4.java
+++ JAVA/pattern4.java
@@ -1,10 +1,10 @@
 public class pattern4 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 4; i++) {
-            for (int j = 1; j <= 4; j++) {
-                if (j<=i) {
-                    System.out.print(i+" ");
-                }
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i; j++) {
+                // if (j<=i) {
+                // }
+                System.out.print(j+1 +" ");
             }
             System.out.println();
         }
