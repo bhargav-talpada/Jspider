56d7654851248abd2d0255a7cb294317140b0517
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-08-12 20:41:53 +0530

    patterns

--- JAVA/pattern3.java
+++ JAVA/pattern3.java
@@ -1,11 +1,29 @@
+// 1.  1                         2.    1
+//     1 2                             2 2
+//     1 2 3                           3 3 3
+//     1 2 3 4                         4 4 4 4
+//     1 2 3 4 5                       5 5 5 5 5
+
 public class pattern3 {
     public static void main(String[] args) {
+        // for (int i = 1; i <= 5; i++) {
+        //     for (int j = 1; j <= 5; j++) {
+        //         if (i>=j) {
+        //             System.out.print(j + " ");
+        //         }
+        //         else {
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        
         for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= 5; j++) {
+            for (int j = 1; j <= i; j++) {
                 if (i>=j) {
-                    System.out.print(j + " ");
+                    System.out.print(i + " ");
                 }
-                else {
+                else{
                     System.out.print(" ");
                 }
             }
