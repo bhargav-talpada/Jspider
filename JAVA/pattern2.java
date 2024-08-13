3fe1fb1dea021b1515628429ed71a364440b5a37
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-08-13 11:20:46 +0530

    yesterday's pattern

--- JAVA/pattern2.java
+++ JAVA/pattern2.java
@@ -2,14 +2,7 @@ public class pattern2 {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= 5; j++) {
-                // if (i<=j) {
-                //     System.out.print("* ");
-                // }
-                // else{
-                //     System.out.print("  ");
-                // }
-                // System.out.println();
-                if (i+j <= 5+1) {
+                if (i>=j) {
                     System.out.print("* ");
                 }
                 else{
--- JAVA/pattern4.java
+++ JAVA/pattern4.java
@@ -2,11 +2,11 @@ public class pattern4 {
     public static void main(String[] args) {
         int c = 1;
         for (int i = 1; i <=4; i++) {
-            for (int j = 1; j <=i; j++) {
-                
+            for (int j = 1; j <=4; j++) {
+                if (j<=i) {
                     System.out.print(c+" ");
                     c++;
-                
+                }                
             }
             System.out.println();
         }
