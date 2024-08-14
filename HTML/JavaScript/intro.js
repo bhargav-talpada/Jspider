ed3d4cf71a537dbc0636f8ed8b8d5c8ce529c49d
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-08-14 13:30:25 +0530

    variable in js

--- HTML/JavaScript/intro.js
+++ HTML/JavaScript/intro.js
@@ -1,3 +1,23 @@
-console.log("external");
-console.warn("warning");
-console.error("error");
+a = 10; // initialize
+var a;  // Declare
+console.log(a);
+a = "bhargav"; // re-initialize
+console.log(a);
+var a = true; // re-declare
+console.log(a);
+
+console.log("================");
+
+let b;  //declare
+b = 10; // initialize
+console.log(b);
+b = 20; // re-initialize
+console.log(b);
+// let b = 10; // can't re-declare
+
+console.log("================");
+
+const c = 10;;  //declare & initialize
+console.log(c);
+// c = 20;  // can't re-initialize
+// const c = 30; // can't re-declare
