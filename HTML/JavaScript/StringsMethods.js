1683f9c7ec02886326fd667348724cd6f7f7cc4c
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-08-27 19:16:55 +0530

    stringJS

---
+++ HTML/JavaScript/StringsMethods.js
@@ -0,0 +1,29 @@
+let str = "string in js";
+
+//---------- slice -> it give specific string from start index to end index ----------
+// let sub = str.slice(5, 9);
+// console.log(sub);
+
+// ---------- toLowerCase -> it converts uppercase to lowercase ----------
+// let low = str.toLowerCase();
+// console.log(low);
+
+// ---------- toUpperCase -> it convers lower to uppercass ----------
+// let upp = str.toUpperCase();
+// console.log(upp);
+
+// ---------- lastIndexOf -> it give index of specific character from last ----------
+// let index = str.lastIndexOf("i");
+// console.log(index);
+ 
+// ---------- indexOf -> it gives index of specific character from first ----------
+// let index = str.indexOf("i");
+// console.log(index);
+
+// ---------- length -> it gives length of string ----------
+// let len = str.length;
+// console.log(len);
+
+// ---------- chatAt -> if gives element of specific index number ----------
+// let c = str.charAt(10);
+// console.log(c);
