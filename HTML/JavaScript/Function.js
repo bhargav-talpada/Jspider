35e53579351bbf899794a0ed3c64f3a5ea96e8a3
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-08-30 19:15:09 +0530

    fun

--- HTML/JavaScript/Function.js
+++ HTML/JavaScript/Function.js
@@ -1,17 +1,42 @@
-function print(){
-    console.log("Bhargav Talpada");    
-}
-print();
+// ---------- Normal Functions ----------
 
-function add(num1, num2){
-    let sum = num1 + num2;
-    console.log(sum);    
-}
-add(54, 45);
+// function print(){
+//     console.log("Bhargav Talpada");    
+// }
+// print();
 
-function adds(num1, num2, num3) {
-    let sum = num1 + num2 + num3;
-    return sum;
-}
-let a = adds(45, 54, 68);
-console.log(a);
+// function add(num1, num2){
+//     let sum = num1 + num2;
+//     console.log(sum);    
+// }
+// add(54, 45);
+
+// function adds(num1, num2, num3) {
+//     let sum = num1 + num2 + num3;
+//     return sum;
+// }
+// let a = adds(45, 54, 68);
+// console.log(a);
+
+// ---------- Arrow Functions ----------
+
+// let sum = (a,b) => {
+//     return a+b;
+// }
+// console.log(sum(10,50));
+
+// ---------- Callback & First Order & Higher Order Functions ----------
+
+// function add(a,b) {
+//     return a+b;
+// }
+
+// => in above code add function is a first order function because it cannot takes any function as a argument & cannot return any function. 
+
+// function total(calc, a,b) {
+//     return calc(a,b);
+// }
+// console.log(total(add,20, 45));
+
+// => in above code calc & add is a callback function because that both functions passed as a parameter in total function.
+// => total function is a Higher order function because it takes a calc function as a argument and return it.
