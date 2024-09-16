591a0090aea938d72bd0ae86351111ca96811507
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-09-16 23:04:02 +0530

    promise

--- HTML/JavaScript/DOM/Promise.js
+++ HTML/JavaScript/DOM/Promise.js
@@ -1,4 +1,34 @@
-async function data() {
-    let data = await fetch(api);
-    let json = await data.json();
-}
+// async function data() {
+//     let data = await fetch('https://jsonplaceholder.typicode.com/photos');
+//     let jsonRes = await data.json();
+//     console.log(jsonRes);   
+// }
+// data();
+
+
+fetch('https://jsonplaceholder.typicode.com/photos')
+.then(res => res.json())
+.then(pic => {
+    console.log(pic);
+    
+    let body = document.getElementById('img-container');
+    for(let i=0; i<pic.length; i++){
+        // let photo = pic[i];
+        let img = document.createElement('img');
+        img.src = pic[i].url;
+        img.style.height = "100px";
+        img.style.width = "100px";
+        body.appendChild(img);
+    }
+
+    // pic.map((i) => {
+    //     let img = document.createElement('img');
+    //     img.src = i.url;
+    //     img.style.height = "100px";
+    //     img.style.width = "100px";
+    //     body.appendChild(img);    
+    // })
+})
+.catch((err) => {
+    console.log(err);
+})
