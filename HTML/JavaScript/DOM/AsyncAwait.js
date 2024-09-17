92539e2a519ef9ee14bb1541a36e50dda7a362e6
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-09-17 20:24:46 +0530

    d arr in java, async in js

--- HTML/JavaScript/DOM/AsyncAwait.js
+++ HTML/JavaScript/DOM/AsyncAwait.js
@@ -1,23 +1,34 @@
-// data();
-// async function data() {
-//     let data = await fetch('https://jsonplaceholder.typicode.com/users');
-//     let jsonRes = await data.json();
-//     console.log(jsonRes);   
-// }
-
-let a = 10;
-let b = 20;
-function parent() {
-    let c = 30;
-    let d = 40;
-    console.log(a+b+c);
-    function child() {
-        console.log(d);
-        
-        d = 140;
-        console.log(a+b+c+d);
-        
-    }
-    child();
+data();
+async function data() {
+    let body = document.getElementById('img-container');
+    let data = await fetch('https://jsonplaceholder.typicode.com/users');
+    let jsonRes = await data.json();
+    console.log(jsonRes);   
+    jsonRes.map((i) => {
+        let h1 = document.createElement('h1');
+        h1.innerText = i.name;
+        h1.style.fontFamily = 'cursive';
+        h1.style.fontSize = '15px';
+        body.appendChild(h1);
+    })
 }
-parent();
+
+
+// clousers ===> to access parent function variables in child function but can not accss
+//              child function variables in parent function.....
+// let a = 10;
+// let b = 20;
+// function parent() {
+//     let c = 30;
+//     let d = 40;
+//     console.log(a+b+c);
+//     function child() {
+//         console.log(d);
+        
+//         d = 140;
+//         console.log(a+b+c+d);
+        
+//     }
+//     child();
+// }
+// parent();
--- JAVA/Array2D.java
+++ JAVA/Array2D.java
@@ -11,15 +11,66 @@ public class Array2D {
 
         int[][] arr = new int[n][m];
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
+        // get data from user
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[i].length; j++){
                 arr[i][j] = sc.nextInt();
             }
         }
 
+        System.out.println("==============================");
+
+        // print 2d array
         for(int i=0; i<arr.length; i++){
             for(int j=0; j<arr[i].length; j++){
                 System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("==============================");
+        
+        // sum of 2d array
+        int sum = 0;
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[i].length;j++){
+                sum = sum + arr[i][j];
+            }
+        }
+        System.out.println(sum);
+
+        System.out.println("==============================");
+
+        // sum of diagonal element of 2d array
+        int sumLeft = 0;
+        int sumRight = 0;
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[i].length; j++){
+                if (i==j) {
+                    sumLeft = sumLeft + arr[i][j];
+                }
+                if (i+j==arr.length-1) {
+                    sumRight = sumRight + arr[i][j];                    
+                }
+            }
+        }
+        System.out.println("Left : "+ sumLeft);
+        System.out.println("Right : "+ sumRight);
+
+        System.out.println("==============================");
+
+        // transpose of matrix
+        int temp = 0;
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr[i].length; j++){
+                temp = arr[j][i];
+                arr[j][i] = arr[i][j];
+                arr[i][j] = temp; 
+            }
+        }
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[i].length; j++){
+                System.out.print(arr[i][j]+" ");
             }
             System.out.println();
         }
