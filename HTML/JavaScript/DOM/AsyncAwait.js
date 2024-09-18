77877a6b600c3bb40964b7c302c745d3011d32ad
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-09-18 12:15:29 +0530

    data

--- HTML/JavaScript/DOM/AsyncAwait.js
+++ HTML/JavaScript/DOM/AsyncAwait.js
@@ -1,16 +1,22 @@
 data();
 async function data() {
     let body = document.getElementById('img-container');
-    let data = await fetch('https://jsonplaceholder.typicode.com/users');
+    let data = await fetch('https://jsonplaceholder.typicode.com/photos');
     let jsonRes = await data.json();
     console.log(jsonRes);   
+    let div = document.createElement('div');
+    div.style.display = 'grid';
+    div.style.gridTemplateColumns = 'auto auto auto auto auto auto';
+    div.style.padding = '20px';
+    div.style.gap = '15px';
     jsonRes.map((i) => {
-        let h1 = document.createElement('h1');
-        h1.innerText = i.name;
-        h1.style.fontFamily = 'cursive';
-        h1.style.fontSize = '15px';
-        body.appendChild(h1);
+        let img = document.createElement('img');
+        img.src = i.url;
+        img.style.height = '200px';
+        img.style.width = '200px';
+        div.appendChild(img);
     })
+    body.appendChild(div);
 }
 
 
