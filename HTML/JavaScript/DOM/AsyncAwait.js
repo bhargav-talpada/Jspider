03f354204bb24d6150665598d86635e67d4710f5
Bhargav Talpada <119996397+bhargav-talpada@users.noreply.github.com>
2024-09-28 17:12:25 +0530

    Update AsyncAwait.js

--- HTML/JavaScript/DOM/AsyncAwait.js
+++ HTML/JavaScript/DOM/AsyncAwait.js
@@ -1,4 +1,5 @@
-data();
+off
+    data();
 async function data() {
     let body = document.getElementById('img-container');
     let data = await fetch('https://jsonplaceholder.typicode.com/photos');
